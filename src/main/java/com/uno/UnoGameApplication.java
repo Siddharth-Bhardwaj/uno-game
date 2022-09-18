b8f72bab2db2e9f9091128afa5566c9a46ced66a
Siddharth Bhardwaj <39260525+Siddharth-Bhardwaj@users.noreply.github.com>
2022-09-18 16:20:23 +0530

    mongodb config (#14)

--- src/main/java/com/uno/UnoGameApplication.java
+++ src/main/java/com/uno/UnoGameApplication.java
@@ -3,11 +3,12 @@ package com.uno;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.data.mongodb.config.EnableMongoAuditing;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableSwagger2
-@EnableMongoAuditing
+//@EnableMongoAuditing
+@EnableWebMvc
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class UnoGameApplication {
 
