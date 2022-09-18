3bd046c269ef29c5f2f6e5ec243e1b2f1e839d1c
Siddharth Bhardwaj <siddharth.bhardwaj2051999@gmail.com>
2022-09-18 17:39:19 +0530

    mongodb config

--- src/main/java/com/uno/repository/GameRepository.java
+++ src/main/java/com/uno/repository/GameRepository.java
@@ -1,10 +1,10 @@
 package com.uno.repository;
 
 import com.uno.model.Game;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface GameRepository extends CrudRepository<Game, String> {
+public interface GameRepository extends MongoRepository<Game, String> {
 
 }
