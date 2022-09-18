00e412a99c4253b42b550b1bdb26840cb340ab57
Siddharth Bhardwaj <siddharth.bhardwaj2051999@gmail.com>
2022-09-18 18:21:12 +0530

    mongodb config

--- src/main/java/com/uno/UnoGameApplication.java
+++ src/main/java/com/uno/UnoGameApplication.java
@@ -3,6 +3,8 @@ package com.uno;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -12,8 +14,8 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableMongoAuditing
 @EnableWebMvc
 @EnableMongoRepositories
-@SpringBootApplication
-@EnableAutoConfiguration
+@SpringBootApplication(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
+@EnableAutoConfiguration(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
 public class UnoGameApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/uno/config/MongoConfig.java
+++ src/main/java/com/uno/config/MongoConfig.java
@@ -1,39 +1,44 @@
-//package com.uno.config;
-//
-//import com.mongodb.WriteConcern;
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.boot.autoconfigure.mongo.MongoProperties;
-//import org.springframework.boot.context.properties.ConfigurationProperties;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.context.annotation.Primary;
-//import org.springframework.data.mongodb.MongoDatabaseFactory;
-//import org.springframework.data.mongodb.core.MongoTemplate;
-//import org.springframework.data.mongodb.core.SimpleMongoClientDatabaseFactory;
-//import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
-//
-//@Slf4j
-//@Configuration
-//public class MongoConfig {
-//
-//    @Primary
-//    @Bean(name = "unoMongoProperties")
-//    @ConfigurationProperties(prefix = "spring.data.mongodb")
-//    MongoProperties getUnoMongoConfig() {
-//        return new MongoProperties();
-//    }
-//
-//    @Bean(name = "mongoTemplate")
-//    public MongoTemplate mongoTemplate() {
-//        log.info("Creating mongodbfactory with uri {}", getUnoMongoConfig().getUri());
-//        MongoDatabaseFactory mongoDatabaseFactory = new SimpleMongoClientDatabaseFactory(getUnoMongoConfig().getUri());
-//        log.info("Created mongodbfactory");
-//        MongoTemplate mongoTemplate = new MongoTemplate(mongoDatabaseFactory);
-//        MappingMongoConverter mongoMapping = (MappingMongoConverter) mongoTemplate.getConverter();
-//        mongoMapping.setMapKeyDotReplacement("#");
-//        mongoMapping.afterPropertiesSet();
-//        mongoTemplate.setWriteConcern(WriteConcern.MAJORITY);
-//        return mongoTemplate;
-//    }
-//
-//}
+package com.uno.config;
+
+import com.mongodb.WriteConcern;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.mongo.MongoProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.SimpleMongoClientDatabaseFactory;
+import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
+
+@Slf4j
+@Configuration
+public class MongoConfig extends AbstractMongoClientConfiguration {
+
+    @Primary
+    @Bean(name = "unoMongoProperties")
+    @ConfigurationProperties(prefix = "spring.data.mongodb")
+    MongoProperties getUnoMongoConfig() {
+        return new MongoProperties();
+    }
+
+    @Bean(name = "mongoTemplate")
+    public MongoTemplate mongoTemplate() {
+        log.info("Creating mongodbfactory with uri {}", getUnoMongoConfig().getUri());
+        MongoDatabaseFactory mongoDatabaseFactory = new SimpleMongoClientDatabaseFactory(getUnoMongoConfig().getUri());
+        log.info("Created mongodbfactory");
+        MongoTemplate mongoTemplate = new MongoTemplate(mongoDatabaseFactory);
+        MappingMongoConverter mongoMapping = (MappingMongoConverter) mongoTemplate.getConverter();
+        mongoMapping.setMapKeyDotReplacement("#");
+        mongoMapping.afterPropertiesSet();
+        mongoTemplate.setWriteConcern(WriteConcern.MAJORITY);
+        return mongoTemplate;
+    }
+
+    @Override
+    protected String getDatabaseName() {
+        return "uno";
+    }
+}
