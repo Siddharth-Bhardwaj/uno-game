117500766c10046ac97cd8815b9abab96d05d5a8
Siddharth Bhardwaj <39260525+Siddharth-Bhardwaj@users.noreply.github.com>
2022-09-18 16:13:16 +0530

    mongodb config (#13)

--- src/main/java/com/uno/UnoGameApplication.java
+++ src/main/java/com/uno/UnoGameApplication.java
@@ -2,22 +2,13 @@ package com.uno;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
-import org.springframework.boot.autoconfigure.mongo.MongoReactiveAutoConfiguration;
-import org.springframework.boot.autoconfigure.mongo.embedded.EmbeddedMongoAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableSwagger2
 @EnableMongoAuditing
-@EnableWebMvc
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, MongoAutoConfiguration.class,
-        MongoDataAutoConfiguration.class, EmbeddedMongoAutoConfiguration.class, MongoReactiveAutoConfiguration.class,
-        HibernateJpaAutoConfiguration.class})
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class UnoGameApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/uno/config/MongoConfig.java
@@ -0,0 +1,7 @@
+package com.uno.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MongoConfig {
+}
