e91da44004a229e408fedaea4278728e695c132a
Siddharth Bhardwaj <siddharth.bhardwaj2051999@gmail.com>
2022-09-18 16:20:05 +0530

    mongodb config

--- src/main/java/com/uno/model/AuditModel.java
+++ src/main/java/com/uno/model/AuditModel.java
@@ -6,16 +6,13 @@ import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import javax.persistence.EntityListeners;
 import java.time.Instant;
 
 @Data
 @SuperBuilder
 @NoArgsConstructor
 @AllArgsConstructor
-@EntityListeners(AuditingEntityListener.class)
 public class AuditModel {
     @CreatedDate
     private Instant createdAt;
