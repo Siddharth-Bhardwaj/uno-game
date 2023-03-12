ef08d97bcfa8e9bc2b922e9b831638f8016d6ad9
Siddharth Bhardwaj <siddharth.bhardwaj2051999@gmail.com>
2023-03-12 14:25:34 +0530

    cors header testing

--- src/main/java/com/uno/config/WebConfig.java
+++ src/main/java/com/uno/config/WebConfig.java
@@ -10,6 +10,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
   @Override
   public void addCorsMappings(CorsRegistry registry) {
-    registry.addMapping("/**").allowedHeaders("*").exposedHeaders("*");
+    registry
+        .addMapping("/**")
+        .allowedOrigins("*")
+        .allowedMethods("GET", "PUT", "POST", "PATCH", "DELETE", "OPTIONS");
   }
 }
