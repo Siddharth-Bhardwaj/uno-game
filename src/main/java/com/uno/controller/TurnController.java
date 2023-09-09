c70c8e7a4092022571b009c2573e6dc658811709
Siddharth-Bhardwaj <siddharth.bhardwaj2051999@gmail.com>
2023-09-08 23:18:03 -0400

    sockJS testing

--- src/main/java/com/uno/controller/TurnController.java
+++ src/main/java/com/uno/controller/TurnController.java
@@ -1,6 +1,9 @@
 package com.uno.controller;
 
 import com.uno.model.Game;
+import com.uno.service.GameService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
@@ -8,10 +11,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("v1.0/turn")
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class TurnController {
+
+  private final GameService gameService;
+
   @MessageMapping
   @SendTo("/topic/turn")
   public Game executeTurn(String gameId) {
-    return null;
+    return gameService.getGame(gameId);
   }
 }
